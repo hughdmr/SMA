bfc781eb3b067537029d529fcf1f9242af1a02fc
Hugues d'Hardemare <hugues.du-moulinet-dhardemare@student-cs.fr>
2026-03-16 15:07:44 +0100

    init objects agents

--- 2_robot_mission_MAS2026/objects.py
+++ 2_robot_mission_MAS2026/objects.py
@@ -1,1 +1,20 @@
 #2 16/03/2026 Hugues d'Hardemare Louis Vauterin
+
+from mesa import Agent
+
+class wasteAgent(Agent):
+    """Agent representing a piece of waste in the environment. The 'zone' attribute indicates in which zone the waste is located (0, 1, or 2)."""
+    def __init__(self, model, zone):
+        super().__init__(model)
+        self.zone = zone
+    
+class radioactivityAgent(Agent):
+    def __init__(self, model, zone):
+        super().__init__(model)
+        self.zone = zone
+
+class wasteDisposalAgent(Agent):
+    def __init__(self, model, zone):
+        super().__init__(model)
+        self.zone = zone
+
